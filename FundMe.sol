b6cd44c7691d908554bd684af74c340e53125660
Iftikhar uddin <iftikhar@connextar.com>
2023-09-25 22:28:46 +0500

    Initial commit

---
+++ FundMe.sol
@@ -0,0 +1,7 @@
+// Crowdfunding Smart Contract
+// This repository contains the source code for a Crowdfunding smart contract, which serves as a decentralized funding platform.
+// Users can contribute funds to support specific initiatives or projects,
+/// and the contract's owner has the ability to withdraw the accumulated amount for the intended purpose.
+/// The contract includes features such as fund management, withdrawal authorization, and transparency.
+
+
