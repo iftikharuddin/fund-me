70104fa1ba6eabc7bbc5e1fa46677f3d37c34364
Iftikhar uddin <iftikhar@connextar.com>
2023-09-26 18:53:10 +0500

    Ready contract where user can send fund and owner can withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -4,4 +4,62 @@
 /// and the contract's owner has the ability to withdraw the accumulated amount for the intended purpose.
 /// The contract includes features such as fund management, withdrawal authorization, and transparency.
 
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.19; // version
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public minAmount = 5e18; // 18 decimels
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address private immutable i_owner;
+
+    // Modifiers
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // receive ETH/Dollars
+        require(msg.value.getConversionRate() >= minAmount, "not enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    // Func to withdraw fund from contract
+    function withdraw() public onlyOwner {
+        for ( uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++ ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+        // Transfer vs call vs Send
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    // receive & fallback
+    // receive and fallback don't need function keyword and are special functions. Construct too
+    // If data is passed in a trx receive func will be called and if not fallback will be called
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
